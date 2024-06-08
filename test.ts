5c9075a1047ff48ab248b78d8cbff7486895d73c
nikelborm <29357907+nikelborm@users.noreply.github.com>
2024-06-08 08:14:09 +0300

    save

--- test.ts
+++ test.ts
@@ -1,9 +1,11 @@
 import test, { describe, it } from 'node:test';
+import { deepStrictEqual } from 'node:assert/strict';
 
 const _ = Symbol('emptiness');
 type _ = typeof _;
+type V = { id: number, v: number }
 
-const A = new Set([
+const FilledA = new Set([
   { id: 1, v: 6 },
   { id: 2, v: 6 },
   { id: 3, v: 7 },
@@ -11,7 +13,7 @@ const A = new Set([
   { id: 5, v: 9 }
 ]);
 
-const B = new Set([
+const FilledB = new Set([
   { id: 1, v: 7  },
   { id: 2, v: 7  },
   { id: 3, v: 8  },
@@ -19,62 +21,55 @@ const B = new Set([
   { id: 5, v: 10 }
 ]);
 
-describe('Full A join empty B', () => {
+const EmptyA = new Set([]);
+
+const EmptyB = new Set([]);
+
+const func = (a, b, c) => new Set([]);
+
+type LNA<L, R> = [L, _];
+type NRA<L, R> = [_, R];
+
+describe('Filled A join empty B', () => {
   test('select * from a left  outer join b using (v);', () => {
-    const ideal = new Set([
-      [{ id: 1, v: 6 }, _],
-      [{ id: 2, v: 6 }, _],
-      [{ id: 3, v: 7 }, _],
-      [{ id: 4, v: 7 }, _],
-      [{ id: 5, v: 9 }, _],
-    ]);
-    const result = func();
+    const ideal = new Set([...FilledA].map(e => [e, _] as LNA<V, V>));
+    const result = func(FilledA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a right outer join b using (v);', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(FilledA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a full  outer join b using (v);', () => {
-    const ideal = new Set([
-      [{ id: 1, v: 6 }, _],
-      [{ id: 2, v: 6 }, _],
-      [{ id: 3, v: 7 }, _],
-      [{ id: 4, v: 7 }, _],
-      [{ id: 5, v: 9 }, _],
-    ]);
-    const result = func();
+    const ideal = new Set([...FilledA].map(e => [e, _] as LNA<V, V>));
+    const result = func(FilledA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a inner       join b using (v);', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(FilledA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a cross       join b;', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(FilledA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a left  outer join b using (v) where b.v is null;', () => {
-    const ideal = new Set([
-      [{ id: 1, v: 6 }, _],
-      [{ id: 2, v: 6 }, _],
-      [{ id: 3, v: 7 }, _],
-      [{ id: 4, v: 7 }, _],
-      [{ id: 5, v: 9 }, _],
-    ]);
-    const result = func();
+    const ideal = new Set([...FilledA].map(e => [e, _] as LNA<V, V>));
+    const result = func(FilledA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a right outer join b using (v) where a.v is null;', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(FilledA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a full  outer join b using (v) where a.v is null or b.v is null;', () => {
-    const ideal = new Set([
-      [{ id: 1, v: 6 }, _],
-      [{ id: 2, v: 6 }, _],
-      [{ id: 3, v: 7 }, _],
-      [{ id: 4, v: 7 }, _],
-      [{ id: 5, v: 9 }, _],
-    ]);
-    const result = func();
+    const ideal = new Set([...FilledA].map(e => [e, _] as LNA<V, V>));
+    const result = func(FilledA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
 })
 
@@ -82,59 +77,43 @@ describe('Full A join empty B', () => {
 describe('Empty A join full B', () => {
   test('select * from a left  outer join b using (v);', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, FilledB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a right outer join b using (v);', () => {
-    const ideal = new Set([
-      [_, { id: 1, v: 7  }],
-      [_, { id: 2, v: 7  }],
-      [_, { id: 3, v: 8  }],
-      [_, { id: 4, v: 8  }],
-      [_, { id: 5, v: 10 }],
-    ]);
-    const result = func();
+    const ideal = new Set([...FilledB].map(e => [_, e] as NRA<V, V>));
+    const result = func(EmptyA, FilledB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a full  outer join b using (v);', () => {
-    const ideal = new Set([
-      [_, { id: 1, v: 7  }],
-      [_, { id: 2, v: 7  }],
-      [_, { id: 3, v: 8  }],
-      [_, { id: 4, v: 8  }],
-      [_, { id: 5, v: 10 }],
-    ]);
-    const result = func();
+    const ideal = new Set([...FilledB].map(e => [_, e] as NRA<V, V>));
+    const result = func(EmptyA, FilledB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a inner       join b using (v);', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, FilledB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a cross       join b;', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, FilledB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a left  outer join b using (v) where b.v is null;', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, FilledB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a right outer join b using (v) where a.v is null;', () => {
-    const ideal = new Set([
-      [_, { id: 1, v: 7  }],
-      [_, { id: 2, v: 7  }],
-      [_, { id: 3, v: 8  }],
-      [_, { id: 4, v: 8  }],
-      [_, { id: 5, v: 10 }],
-    ]);
-    const result = func();
+    const ideal = new Set([...FilledB].map(e => [_, e] as NRA<V, V>));
+    const result = func(EmptyA, FilledB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a full  outer join b using (v) where a.v is null or b.v is null;', () => {
-    const ideal = new Set([
-      [_, { id: 1, v: 7  }],
-      [_, { id: 2, v: 7  }],
-      [_, { id: 3, v: 8  }],
-      [_, { id: 4, v: 8  }],
-      [_, { id: 5, v: 10 }],
-    ]);
-    const result = func();
+    const ideal = new Set([...FilledB].map(e => [_, e] as NRA<V, V>));
+    const result = func(EmptyA, FilledB, '');
+    deepStrictEqual(result, ideal);
   });
 })
 
@@ -142,34 +121,42 @@ describe('Empty A join full B', () => {
 describe('Empty A join empty B', () => {
   test('select * from a left  outer join b using (v);', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a right outer join b using (v);', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a full  outer join b using (v);', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a inner       join b using (v);', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a cross       join b;', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a left  outer join b using (v) where b.v is null;', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a right outer join b using (v) where a.v is null;', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
   test('select * from a full  outer join b using (v) where a.v is null or b.v is null;', () => {
     const ideal = new Set([]);
-    const result = func();
+    const result = func(EmptyA, EmptyB, '');
+    deepStrictEqual(result, ideal);
   });
 })
