91901cbb415a8f718387d631bdeea966591ad6ee
nikelborm <evadev@duck.com>
2025-02-12 15:18:27 +0000

    save

---
+++ getShuffledArrayFrom.ts
@@ -0,0 +1,19 @@
+export function getShuffledArrayFrom<T>(sourceArray: Array<T>) {
+  const resultArray = [...sourceArray];
+  let currentIndex = resultArray.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [resultArray[currentIndex], resultArray[randomIndex]] = [
+      resultArray[randomIndex]!,
+      resultArray[currentIndex]!,
+    ];
+  }
+
+  return resultArray;
+}
