67959f89888733f1fd3ffced9691886d5fdb35ee
nikelborm <evadev@duck.com>
2025-02-18 15:10:27 +0000

    save

--- helpers.ts
+++ helpers.ts
@@ -1,26 +1,23 @@
 import { _ } from './constants';
-import { BBA, toV, to_ } from './types';
+import { BBA, ToV, To_ } from './types';
 
-export function isEmpty<T>(v: T): v is to_<T> {
+export function isEmpty<T>(v: T): v is To_<T> {
   return v === _;
 }
 
-export function isNotEmpty<T>(v: T): v is toV<T> {
+export function isNotEmpty<T>(v: T): v is ToV<T> {
   return v !== _;
 }
 
 export function castToBBA<L, R>(
-  lr: [unknown, unknown]
+  lr: [unknown, unknown],
 ): asserts lr is BBA<L, R> {
-  // TODO: check l is L and r is R
+  // TODO: check l is L and r is R?
 
   const [l, r] = lr;
 
-  if(
-    (isEmpty   (l) && isNotEmpty(r)) ||
-    (isNotEmpty(l) && isEmpty   (r)) ||
-    (isNotEmpty(l) && isNotEmpty(r))
-  ) return;
-
-  throw new Error(`What the actual fuck??? lr is ${lr}`);
+  if (isEmpty(l) && isEmpty(r))
+    throw new Error(
+      `Error: What the actual fuck??? Failed to cast to BBA because left is empty and right is empty too.`,
+    );
 }
