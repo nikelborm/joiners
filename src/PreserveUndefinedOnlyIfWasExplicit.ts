7048ff387d3f4fbdcfe3be60a4bbcf5574da2ff3
nikelborm <evadev@duck.com>
2025-10-05 17:46:03 +0300

    quick save: Sun Oct  5 17:46:03 MSK 2025

--- src/PreserveUndefinedOnlyIfWasExplicit.ts
+++
@@ -1,5 +0,0 @@
-export type PreserveUndefinedOnlyIfWasExplicit<T, Key extends keyof T> = {
-  [K in Key]: T[Key];
-} extends { [K in Key]: Exclude<T[Key], undefined> }
-  ? Exclude<T[Key], undefined>
-  : T[Key];
--- src/PreserveUndefinedOnlyIfWasExplicit.tstyche.spec.ts
+++
@@ -1,8 +0,0 @@
-import { type Equals, assert } from 'tsafe';
-import type { PreserveUndefinedOnlyIfWasExplicit } from './PreserveUndefinedOnlyIfWasExplicit.ts';
-
-
-assert<Equals<PreserveUndefinedOnlyIfWasExplicit<{ a?: string             }, 'a'>, string             >>;
-assert<Equals<PreserveUndefinedOnlyIfWasExplicit<{ a?: string | undefined }, 'a'>, string | undefined >>;
-assert<Equals<PreserveUndefinedOnlyIfWasExplicit<{ a : string             }, 'a'>, string             >>;
-assert<Equals<PreserveUndefinedOnlyIfWasExplicit<{ a : string | undefined }, 'a'>, string | undefined >>;
